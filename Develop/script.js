b1144b1417c78d08d8bdfdbeed954616d0d39047
Jacob Liberty <jacob.tobin.liberty@gmail.com>
2021-12-20 18:56:15 -0500

    used moment to check time and edit styles

--- Develop/script.js
+++ Develop/script.js
@@ -10,27 +10,33 @@ var calendarTime = ["09","10","11","12","13","14","15","16","17"];
 // Create time blocks sections
 for (var i = 0; i < hours; i++){
     console.log("for loop ran");
-    var rowId = `row${i}`;
+    var rowId = `${i}`;
     var row = $('#container').append(`<div class="row time-block" id="${rowId}"></div>`); 
     var time = $(`#${rowId}`).append(`<div class='hour'>${calendarTimeText[i]}<div>`);
-    var text = $(`#${rowId}`).append(`<div class='description id=${calendarTime[i]}'> Test <div>`);
+    var text = $(`#${rowId}`).append(`<input type='text' class='description id=${calendarTime[i]}'></input>`);
     var save = $(`#${rowId}`).append("<div class='saveBtn'><i class='fa fa-save'></i></div>");
 };
 
-// Check time and colour blocks based on time
+// Check time and color blocks based on time
 function checktime(){
-    console.log("checktime")
     var time = moment().format('HH');
     for (var i=0; i<hours; i++){
-        var rowId = `row${i}`;
-        if (parseInt.rowId < time){
-            $('#rowID').addClass("past");
-            console.log("if ran")
+        var timeSlot = 9;
+        var currentBlock = i;
+        if (timeSlot < time){
+            $(`#${currentBlock}`).addClass("past");
         }
+        if (timeSlot === time){
+            $(`#${currentBlock}`).addClass("present");
+        }
+        if (timeSlot > time){
+            $(`#${currentBlock}`).addClass("future");
+        }
+        timeSlot++
     }
 }
 
-
+checktime()
 
 
 // Event listeners 
