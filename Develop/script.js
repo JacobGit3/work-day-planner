f0f8787e9af48a6756a3b6aa53f4d7338cc25098
Jacob Liberty <jacob.tobin.liberty@gmail.com>
2021-12-26 18:31:30 -0500

    Load tasks, final refactor/touch up

--- Develop/script.js
+++ Develop/script.js
@@ -1,37 +1,28 @@
-
-
-// Global Variables
-var hours = 9;
-var schedule = $("container");
-
-
-var calendarTimeText = ["9AM","10AM","11AM","12AM","1PM","2PM","3PM","4PM","5PM"];
-var calendarTime = ["09","10","11","12","13","14","15","16","17"];
-
-
-//Add Current time to top of page
-$('#currentDay').append(moment().format('LLLL'))
+// Global variables
+const hours = 9;
+const calendarTimeText = ["9AM","10AM","11AM","12AM","1PM","2PM","3PM","4PM","5PM"];
+const TEXT_SAVE_DATA_KEY = "textData";
 
 // Create time blocks sections
-for (var i = 0; i < hours; i++){
-    var rowId = `${i}`;
-    var row = $('#container').append(`<div class="row time-block" id="${rowId}"></div>`); 
-    var time = $(`#${rowId}`).append(`<div class='hour'>${calendarTimeText[i]}<div>`);
-    var text = $(`#${rowId}`).append(`<input type='text' class='description id=${i}}'></input>`);
-    var save = $(`#${rowId}`).append(`<button class='saveBtn' id='saveBtn${i}'><i class='fa fa-save'></i></button>`);
+for (let i = 0; i < hours; i++){
+    let rowId = `${i}`;
+    let row = $('#container').append(`<div class="row time-block" id="${rowId}"></div>`); 
+    let time = $(`#${rowId}`).append(`<div class='hour'>${calendarTimeText[i]}<div>`);
+    let text = $(`#${rowId}`).append(`<input type='text' class='description' id='textInput${i}'></input>`);
+    let save = $(`#${rowId}`).append(`<button class='saveBtn' id='saveBtn${i}'><i class='fa fa-save'></i></button>`);
 };
 
 // Check time and colour blocks based on time
 function checktime(){
     // Update time at top of web page
-    var currentTime = moment().format('LLLL');
+    let currentTime = moment().format('LLLL');
     $('#currentDay').innerHTML = currentTime;
 
     // Check time and change color
-    var time = moment().format('HH');
-    var timeBlock = 9;
-    for (var i=0; i<hours; i++){
-        var rowId = `${i}`;
+    let time = moment().format('HH');
+    let timeBlock = 9;
+    for (let i=0; i<hours; i++){
+        let rowId = `${i}`;
         if (timeBlock < time){
             $(`#${rowId}`).addClass("past");
         }
@@ -41,16 +32,15 @@ function checktime(){
         if (timeBlock > time){
             $(`#${rowId}`).addClass("future");
         }
-        timeBlock++
+        timeBlock++;
     }
 }
 
-
 // Function to run checktime on load of page then once every hour
 function hourly() {
-    // set interval variables and call checktime on load
-    var minsInterval = (60 - moment().format('m')) * 1000 * 60;
-    var hour = 1000 * 60 * 60;
+    // set interval letiables and call checktime on load
+    const minsInterval = (60 - moment().format('m')) * 1000 * 60;
+    const hour = 1000 * 60 * 60;
     checktime();
 
     // If time isnt exactly at an hour, set interval to mins to next hour
@@ -63,17 +53,30 @@ function hourly() {
     }
 }
 
-
-function saveThis() {
-    for (var i=0; i<hours; i++) {
-        console.log('saved');
-    }
+// Save text data of the respective input when save button pressed
+function saveTextDataBasedOnIndex(index) {
+    let saveTextElement = document.getElementById(`textInput${index}`).value;
+    // getElementById find which text box we need, get data, save data
+    localStorage.setItem(TEXT_SAVE_DATA_KEY + index, saveTextElement);
 };
+
+// Function to load text from local storgae on page reload
+function loadTextOnPageReload() {
+    for (let i=0; i<hours; i++) {
+        let textBox = document.getElementById(`textInput${i}`);
+        let textContent = localStorage.getItem(TEXT_SAVE_DATA_KEY + i);
+        textBox.value = textContent;
+    }
+}
 
 // Event listeners and handler for save button
-var btns = document.getElementsByClassName('saveBtn${i}');
-for (var i = 0; i < btns.length; i++) {
-    btns[i].addEventListener("click", saveThis)
+for (let i = 0; i < hours; i++) {
+    const saveButton = document.getElementById(`saveBtn${i}`);
+    saveButton.addEventListener("click", function(){ saveTextDataBasedOnIndex(i); });
 };
 
-hourly()
+//Run these functions on load
+hourly();
+loadTextOnPageReload();
+//Add Current time to top of page
+$('#currentDay').append(moment().format('LLLL'));
